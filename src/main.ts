fde9c859a08d6ddf0b693b6b52c0c29077420f11
Jobson Ribeiro <jobson.analistati@gmail.com>
2024-07-04 20:36:42 -0300

    Refactor: Ajusts for ZAP OWASP

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        imgSrc: ["'self'", 'data:'],
+        connectSrc: ["'self'"],
+        fontSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        frameSrc: ["'self'"],
+        baseUri: ["'self'"],
+        formAction: ["'self'"]
+      }
+    }
+  }));
   
   await app.listen(process.env.SERVICE_PORT, () => {
     Logger.log(
